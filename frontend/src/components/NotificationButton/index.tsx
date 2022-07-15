e73271aadeff4ed942534cd964f85c0b7079491f
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-07-15 11:32:13 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -3,6 +3,7 @@ import "./styles.css";
 import icon from "../../assets/img/notification-icon.svg";
 import axios from "axios";
 import { BASE_URL } from "../../utils/request";
+import { toast } from "react-toastify";
 
 type Props = {
   saleId: number;
@@ -10,7 +11,7 @@ type Props = {
 
 function handleClick(id: number) {
   axios(`${BASE_URL}/sales/${id}/notification`).then((response) => {
-    console.log("Deuuu bom!");
+    toast.info("SMS enviado com sucesso!");
   });
 }
 
